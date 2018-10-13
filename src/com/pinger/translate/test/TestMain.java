818974bd968e4a6a77177474446f06dc3348f1f3
Pinger <wanitme@gmail.com>
2018-10-14 00:28:49 +0800

    添加将翻译记录生成文档

--- src/com/pinger/translate/test/TestMain.java
+++ src/com/pinger/translate/test/TestMain.java
@@ -2,6 +2,7 @@ package com.pinger.translate.test;
 
 import com.pinger.translate.bean.TranslationBean;
 import com.pinger.translate.main.LocalData;
+import com.pinger.translate.main.MarkDownGenerate;
 import com.pinger.translate.net.NetWorkUtils;
 import gherkin.deps.com.google.gson.Gson;
 import org.apache.http.util.TextUtils;
@@ -19,9 +20,16 @@ public class TestMain {
         if (!TextUtils.isEmpty(localData)) {
             // TODO 还要判断一下本地数据的code是不是有问题
             TranslationBean bean = new Gson().fromJson(localData, TranslationBean.class);
-
         }
-        NetWorkUtils.requestGet("中文");
+
+        String result = NetWorkUtils.requestGet("中文");
+
+        // 将翻译过的结果存入到MarkDown文档中
+        if (!MarkDownGenerate.isSave("", word)) {
+            MarkDownGenerate.saveWords(word, result, "");
+        }
+
+
     }
 
 
